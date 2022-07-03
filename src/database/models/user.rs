e9927a714ed46bb768cd39419429476e027c993d
11tuvork28 <zofi@xamh.de>
2022-07-03 15:57:41 +0200

    made fields pub

--- src/database/models/user.rs
+++ src/database/models/user.rs
@@ -4,11 +4,11 @@ use scylla::cql_to_rust::FromCqlVal;
 /// Represents a user queried from the database
 #[derive(Debug,IntoUserType, FromUserType,FromRow)]
 pub struct DBUser {
-    uuid: String, // partition key
-    name: String, // clustering key
-    password: String,
-    token: String,
-    feed_needs_update: bool,
+    pub uuid: String, // partition key
+    pub name: String, // clustering key
+    pub password: String,
+    pub token: String,
+    pub feed_needs_update: bool,
 }
 
 impl DBUser {
