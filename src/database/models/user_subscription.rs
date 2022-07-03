768620591208b0cc177df73defdfc2618e7ab91b
11tuvork28 <zofi@xamh.de>
2022-07-03 15:59:16 +0200

    maded fields pub

--- src/database/models/user_subscription.rs
+++ src/database/models/user_subscription.rs
@@ -3,7 +3,7 @@ use scylla::cql_to_rust::FromCqlVal;
 /// Represents a channel that a user has subcribed to 
 #[derive(Debug,IntoUserType, FromUserType,FromRow)]
 pub struct UserSubscribed {
-    uid: String, // partition key
-    subuuid: String, // clustering key
-    channel_id: String,
+    pub uid: String, // partition key
+    pub subuuid: String, // clustering key
+    pub channel_id: String,
 }
